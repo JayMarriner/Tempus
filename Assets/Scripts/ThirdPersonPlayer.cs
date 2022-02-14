354df2f79c23946cb461f9ab233b98c81e32b68a
Kaan Dilmen <60734850+My5ticTurk@users.noreply.github.com>
2022-02-14 15:12:31 +0000

    Controller Updated
    Working a little bit better but still no jump, will work on this for next week.

--- Assets/Scripts/ThirdPersonPlayer.cs
+++ Assets/Scripts/ThirdPersonPlayer.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class ThirdPersonPlayer : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
-    [SerializeField] private Transform camera;
+    [SerializeField] private Transform camera;   
 
     public Transform groundCheck;
     public LayerMask groundMask;
-    public float gravity = -9.81f;
+    [Range(-10, -1)]
+    [SerializeField]public float gravity = -2.81f;
     public float groundDistance = 0.2f;
 
     [SerializeField] private float speed = 6f;
@@ -18,21 +19,14 @@ public class ThirdPersonPlayer : MonoBehaviour
 
     private float turnSmoothVelocity;
     private Vector3 velocity;
-    private bool isGrounded;
+    private bool isGrounded;    
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(isGrounded);
-
-        Cursor.lockState = CursorLockMode.Locked;
-
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f; 
-        }
+        Cursor.lockState = CursorLockMode.Locked;                   
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -46,16 +40,18 @@ public class ThirdPersonPlayer : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
 
-            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            Vector3 moveDir = camera.TransformDirection(dir);
+            velocity.y += gravity;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * 2.0f * gravity);
+            controller.Move(new Vector3(0, jumpHeight, 0));
         }
+        Debug.Log(isGrounded);
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        
     }
+
 }
