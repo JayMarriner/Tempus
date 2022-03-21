5f2d0403c3817fd5774d80553a64f6fd7a720f55
Jay Marriner <jaymarriner123@gmail.com>
2022-03-21 16:32:24 +0000

    Bow work
    Worked on the bow mechanics.

--- Assets/Scripts/Weapons/BowHandler.cs
+++ Assets/Scripts/Weapons/BowHandler.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BowHandler : MonoBehaviour
 {
+    [SerializeField] GameObject arrowPrefab;
+    [SerializeField] GameObject arrowSpawnPoint;
+    GameObject currentArrow;
     LineRenderer line;
     InputManager inputManager;
     bool pullingString;
@@ -25,13 +28,22 @@ public class BowHandler : MonoBehaviour
     {
         if (Input.GetKey(inputManager.shoot) && !pullingString)
             StartCoroutine(PullBow());
+        else if (pullingString && Input.GetKeyUp(inputManager.shoot))
+            StartCoroutine(RelaxBow());
+        else if (pullingString && Input.GetKeyUp(inputManager.aim))
+            StartCoroutine(RelaxBow());
+
+        if (!pullingString && currentArrow == null)
+            currentArrow = Instantiate(arrowPrefab, arrowSpawnPoint.transform);
     }
 
     IEnumerator PullBow()
     {
         moveAmt = 0;
+        if (currentArrow == null)
+            currentArrow = Instantiate(arrowPrefab, arrowSpawnPoint.transform);
         pullingString = true;
-        while (moveAmt < 0.1f)
+        while (moveAmt < 2f)
         {
             print(moveAmt);
             //If the player is still holding the fire button then continue to pull the bowstring back until max amount.
@@ -40,20 +52,22 @@ public class BowHandler : MonoBehaviour
                 //Move back the midpoint of the bowstring line renderer, move arrow back equal amount.
                 line.SetPosition(1, new Vector3(line.GetPosition(1).x, line.GetPosition(1).y, line.GetPosition(1).z - 0.01f));
 
+                //Move arrow back with string being pulled.
+                currentArrow.transform.localPosition += new Vector3(0, 0, -0.01f);
+
                 //Update moveamt to break the while loop.
                 moveAmt += 0.01f;
 
                 //Increase the powerAmt multiplier to increase power as the bow is pulled further back.
                 powerAmt = moveAmt * 10;
 
-                yield return new WaitForSeconds(0.02f);
+                yield return new WaitForSeconds(0.001f);
             }
             else
             {
                 StartCoroutine(RelaxBow());
                 break;
             }
-            pullingString = false;
         }
     }
 
@@ -65,9 +79,11 @@ public class BowHandler : MonoBehaviour
         //Allow bow to return back to normal position over small amount of time .
         while (moveAmt > 0f)
         {
-            line.SetPosition(1, new Vector3(line.GetPosition(1).x, line.GetPosition(1).y, line.GetPosition(1).z - 0.01f));
-            moveAmt -= 0.02f;
-            yield return new WaitForSeconds(0.0005f);
+            line.SetPosition(1, new Vector3(line.GetPosition(1).x, line.GetPosition(1).y, line.GetPosition(1).z + 0.04f));
+            if(currentArrow != null)
+                currentArrow.transform.localPosition += new Vector3(0, 0, 0.01f);
+            moveAmt -= 0.04f;
+            yield return new WaitForSeconds(0.00001f);
         }
 
         //Pulling string set to false, bow is no longer being pulled at this point.
@@ -75,6 +91,8 @@ public class BowHandler : MonoBehaviour
 
         //Set bow back to the initial position, without this the bow slowly goes further and further in with each use.
         line.SetPosition(1, initialPos);
+        if (currentArrow != null)
+            currentArrow.transform.position = arrowSpawnPoint.transform.position;
 
         //Cooldown for the reload on arrow.
         yield return new WaitForSeconds(1f);
