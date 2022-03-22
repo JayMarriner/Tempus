d6f1eb83c643e56fde338b9ba3f62d552314029f
Jay Marriner <jaymarriner123@gmail.com>
2022-03-22 19:08:23 +0000

    Arrow firing and compiler issue fixes
    Arrows now fly when shoot button is released. Compiler issues have been removed (from obsolete packages) to allow building of project.
    
    Collision with enemies still needs to be added.

--- Assets/Scripts/Weapons/BowHandler.cs
+++ Assets/Scripts/Weapons/BowHandler.cs
@@ -6,6 +6,8 @@ public class BowHandler : MonoBehaviour
 {
     [SerializeField] GameObject arrowPrefab;
     [SerializeField] GameObject arrowSpawnPoint;
+    [Range(5000,20000)]
+    [SerializeField] int arrowSpeed;
     GameObject currentArrow;
     LineRenderer line;
     InputManager inputManager;
@@ -14,6 +16,7 @@ public class BowHandler : MonoBehaviour
     Vector3 initialPos;
     float powerAmt;
     float moveAmt;
+    bool readyToFire;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +29,18 @@ public class BowHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (readyToFire && Input.GetKeyUp(inputManager.shoot))
+            ShootArrow();
+
+        if (!gameObject.activeSelf)
+            StartCoroutine(RelaxBow());
+
+        if (Input.GetKey(inputManager.aim))
+            gameObject.transform.eulerAngles = new Vector3(Camera.main.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, gameObject.transform.eulerAngles.z);
+        else
+            gameObject.transform.localEulerAngles = new Vector3(0, 0, 90);
+
+
         if (Input.GetKey(inputManager.shoot) && !pullingString)
             StartCoroutine(PullBow());
         else if (pullingString && Input.GetKeyUp(inputManager.shoot))
@@ -37,31 +52,44 @@ public class BowHandler : MonoBehaviour
             currentArrow = Instantiate(arrowPrefab, arrowSpawnPoint.transform);
     }
 
+    void ShootArrow()
+    {
+        Rigidbody arrowRb = currentArrow.GetComponent<Rigidbody>();
+        currentArrow.GetComponent<BoxCollider>().isTrigger = false;
+        currentArrow.GetComponent<ArrowHandler>().isFired = true;
+        arrowRb.AddForce(transform.forward * Time.deltaTime * arrowSpeed * powerAmt * 2);
+        currentArrow.transform.parent = null;
+        arrowRb.useGravity = true;
+        currentArrow = null;
+    }
+
     IEnumerator PullBow()
     {
         moveAmt = 0;
         if (currentArrow == null)
             currentArrow = Instantiate(arrowPrefab, arrowSpawnPoint.transform);
         pullingString = true;
-        while (moveAmt < 2f)
+        while (moveAmt < 1.75f)
         {
-            print(moveAmt);
+            if (moveAmt > 0.5f && !readyToFire)
+                readyToFire = true;
+
             //If the player is still holding the fire button then continue to pull the bowstring back until max amount.
             if (Input.GetKey(inputManager.shoot) && Input.GetKey(inputManager.aim))
             {
                 //Move back the midpoint of the bowstring line renderer, move arrow back equal amount.
-                line.SetPosition(1, new Vector3(line.GetPosition(1).x, line.GetPosition(1).y, line.GetPosition(1).z - 0.01f));
+                line.SetPosition(1, new Vector3(line.GetPosition(1).x, line.GetPosition(1).y, line.GetPosition(1).z - 0.02f));
 
                 //Move arrow back with string being pulled.
-                currentArrow.transform.localPosition += new Vector3(0, 0, -0.01f);
+                currentArrow.transform.localPosition += new Vector3(0, 0, -0.02f);
 
                 //Update moveamt to break the while loop.
-                moveAmt += 0.01f;
+                moveAmt += 0.02f;
 
                 //Increase the powerAmt multiplier to increase power as the bow is pulled further back.
                 powerAmt = moveAmt * 10;
 
-                yield return new WaitForSeconds(0.001f);
+                yield return new WaitForSeconds(0.0001f);
             }
             else
             {
@@ -73,16 +101,17 @@ public class BowHandler : MonoBehaviour
 
     IEnumerator RelaxBow()
     {
+        readyToFire = false;
         //Bow is currently being relaxed, so set to true. bow can't be pulled until cooldown ends.
         relaxingBow = true;
 
         //Allow bow to return back to normal position over small amount of time .
         while (moveAmt > 0f)
         {
-            line.SetPosition(1, new Vector3(line.GetPosition(1).x, line.GetPosition(1).y, line.GetPosition(1).z + 0.04f));
+            line.SetPosition(1, new Vector3(line.GetPosition(1).x, line.GetPosition(1).y, line.GetPosition(1).z + 0.08f));
             if(currentArrow != null)
-                currentArrow.transform.localPosition += new Vector3(0, 0, 0.01f);
-            moveAmt -= 0.04f;
+                currentArrow.transform.localPosition += new Vector3(0, 0, 0.08f);
+            moveAmt -= 0.08f;
             yield return new WaitForSeconds(0.00001f);
         }
 
@@ -98,5 +127,15 @@ public class BowHandler : MonoBehaviour
         yield return new WaitForSeconds(1f);
 
         relaxingBow = false;
+    }
+
+    private void OnEnable()
+    {
+        if(currentArrow != null)
+        {
+            StartCoroutine(RelaxBow());
+            //currentArrow.transform.position = arrowSpawnPoint.transform.position;
+            //line.SetPosition(1, initialPos);
+        }
     }
 }
