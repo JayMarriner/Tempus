370c04af76948e1f5ff07fac930f804f1c053020
My5ticTurk <lucakaan1@yahoo.co.uk>
2022-05-05 20:17:19 +0100

    Tutorial Level Complete
    All that needs to be added now is the correct voice lines and animation. Will discuss the use of different assets throughout the level in due time.

--- Assets/Scripts/Enemies/RobotInfo.cs
+++ Assets/Scripts/Enemies/RobotInfo.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.AI;
+using Cinemachine;
 
 public class RobotInfo : MonoBehaviour
 {
     [SerializeField] float health = 100;
-    [SerializeField] Image healthImg;
-    [SerializeField] GameObject door;
-    [SerializeField] NavMeshAgent agent;
+    [SerializeField] private Image healthImg;
+    [SerializeField] private GameObject portal;
+    [SerializeField] private CinemachineVirtualCamera cam;
+    [SerializeField] private GameObject portal_NPC;
+    
     public float GetHealth { get => health; }
     public Shooter shooterScript;
     ThirdPersonPlayer player;
@@ -18,7 +21,9 @@ public class RobotInfo : MonoBehaviour
 
     private void Start()
     {
+        portal.gameObject.SetActive(false);
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonPlayer>();
+        portal_NPC.gameObject.SetActive(false);
     }
 
     public void LowerHealth(int amt)
@@ -28,14 +33,17 @@ public class RobotInfo : MonoBehaviour
 
         if(health <= 0)
         {          
-            StartCoroutine(StartLoadToPortalRoom());                         
+            StartCoroutine(StartLoadToPortalRoom());
+            cam.Priority = 20;
+            portal.gameObject.SetActive(true);
+            portal_NPC.gameObject.SetActive(true);
         }
     }
 
     IEnumerator StartLoadToPortalRoom()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);        
+        cam.Priority = 0;
         Destroy(gameObject);
-        SceneManager.LoadScene(1);        
     }
 }
